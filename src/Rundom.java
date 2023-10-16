74efb68e884e6dd16b08e3f03a6855aaaa7c33e1
Deadpool 1652 <iyanovskiy.08@gmail.com>
2023-10-16 17:00:30 +0100

    Initial commit

---
+++ src/Rundom.java
@@ -0,0 +1,11 @@
+
+public class Rundom {
+    public class Rundom {
+        public static void main(String[] args){
+            for (int i=1; i<=9; i++);
+            for(int i=1; i<10; i++);
+            System.out.println("Illia")
+        }
+    }
+
+}
---
+++ src/RundomNumber.java
@@ -0,0 +1,8 @@
+package Hmework1;
+
+public class RundomNumber {
+    public static void main(String[] args){
+        double a = Math.random()*3;
+
+        System.out.println(a);
+    }
